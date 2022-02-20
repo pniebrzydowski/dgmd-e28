f3d59e9946195190faed2d37c4730c02ca91cee1
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2022-02-20 18:32:52 -0300

    add other win states

--- assignment-02/script.js
+++ assignment-02/script.js
@@ -1,4 +1,5 @@
-let currentPlayer = 'x';
+let currentPlayer = "x";
+let winOptions = [];
 let winner = null;
 let fullSquares = 0;
 let gameBoardState;
@@ -7,111 +8,130 @@ let $currentPlayerContainer;
 
 const logCurrentBoardState = () => {
   const logValues = [];
-  gameBoardState.forEach(row => {
+  gameBoardState.forEach((row) => {
     const rowValues = [];
-    row.forEach($space => {
+    row.forEach(($space) => {
       rowValues.push(getSpaceValue($space));
     });
-    logValues.push(` ${rowValues.join(' | ')} `);
-  })
+    logValues.push(` ${rowValues.join(" | ")} `);
+  });
   console.log("Current Board State:");
-  console.log(logValues.join('\n-----------\n'));
-}
+  console.log(logValues.join("\n-----------\n"));
+};
 
 const updateCurrentPlayer = () => {
-  currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+  currentPlayer = currentPlayer === "x" ? "o" : "x";
   updateCurrentPlayerMsg();
-}
+};
 
 const isStalemate = () => fullSquares === 9;
 const isGameOver = () => isStalemate() || !!winner;
 const getSpaceValue = ($space) => $space.dataset.value;
 const resetSpaceValue = ($space) => {
-  $space.dataset.value = ' ';
-  $space.firstElementChild.innerHTML = '';
-}
+  $space.dataset.value = " ";
+  $space.firstElementChild.innerHTML = "";
+};
 
-const showNewGameState = (msg = '', className = '') => {
+const showNewGameState = (msg = "", className = "") => {
   $gameStateContainer.className = className;
   $gameStateContainer.innerHTML = msg;
-}
+};
 
 const showNewGameError = (msg) => {
-  showNewGameState(msg, 'error');
-}
+  showNewGameState(msg, "error");
+};
 
-const updateCurrentPlayerMsg = () =>
-  $currentPlayerContainer.innerHTML = `${currentPlayer}, it's your turn!`
+const updateCurrentPlayerMsg = () => ($currentPlayerContainer.innerHTML = `${currentPlayer}, it's your turn!`);
 
-/**
- * win states
- *
- * [0,0], [0,1], [0,2]
- * [1,0], [1,1], [1,2]
- * [2,0], [2,1], [2,2]
- * 
- * [0,0], [1,0], [2,0]
- * [0,1], [1,1], [2,1]
- * [0,2], [1,2], [2,2]
- * 
- * [0,0], [1,1], [2,2]
- * [2,0], [1,1], [0,2]
- * */
+const buildWinOptions = () => {
+  let wins = [];
+  const diagWin = [];
+  for (let x = 0; x < 3; x++) {
+    const rowWin = [];
+    const colWin = [];
+    for (let y = 0; y < 3; y++) {
+      rowWin.push([x, y]);
+      colWin.push([y, x]);
+    }
+    diagWin.push([x, x]);
+    wins.push(rowWin);
+    wins.push(colWin);
+  }
+
+  return [
+    ...wins,
+    diagWin,
+    [
+      [2, 0],
+      [1, 1],
+      [0, 2],
+    ],
+  ];
+};
+
+const isWin = () => {
+  let isWin = false;
+  winOptions.forEach((winSet) => {
+    let points = 0;
+    winSet.forEach((spaceCoordinates) => {
+      const x = spaceCoordinates[0];
+      const y = spaceCoordinates[1];
+      const $space = gameBoardState[x][y];
+      const spaceValue = getSpaceValue($space);
+
+      if (spaceValue !== currentPlayer) {
+        return;
+        // not a winner
+      }
+      points++;
+    });
+
+    if (points === 3) {
+      isWin = true;
+      return;
+    }
+  });
+
+  return isWin;
+};
 
 const checkForWin = () => {
-  let winState = false;
-  // check for row wins
-  for(let row = 0; row < 3; row++) {
-    let rowPoints = 0;
-    for(let col = 0; col < 3; col++) {
-      const spaceValue = getSpaceValue(gameBoardState[row][col]);
-      if (spaceValue !== currentPlayer) {
-        // row is not a winner
-        rowWins = false;
-        break;
-      }
-      rowPoints++;
-    }
-    if (rowPoints === 3) {
-      winState = true;
-      break;
-    }
-  }
+  const winState = isWin();
 
   if (winState === true) {
-    showNewGameState(`Congratulations player ${currentPlayer}, you've won the game!`, 'success');
+    showNewGameState(`Congratulations player ${currentPlayer}, you've won the game!`, "success");
     winner = currentPlayer;
   } else if (fullSquares === 9) {
-    showNewGameState('Stalemate!')
+    showNewGameState("Stalemate!");
   }
   return winState;
-}
+};
 
-const clearBoard = () => {
+const resetGame = () => {
   winner = null;
-  currentPlayer = 'x';
+  currentPlayer = "x";
   fullSquares = 0;
   showNewGameState();
-  for(let row = 0; row < 3; row++) {
-    for(let col = 0; col < 3; col++) {
+  updateCurrentPlayerMsg();
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
       resetSpaceValue(gameBoardState[row][col]);
     }
-  };
-}
+  }
+};
 
 const onSpaceClick = ($space) => {
   if (isGameOver()) {
-    console.log('game is over');
-    if (confirm('The game is already over, do you want to start over?')) {
-      clearBoard();
+    console.log("game is over");
+    if (confirm("The game is already over, do you want to start over?")) {
+      resetGame();
     }
     return;
   }
-  console.log('click');
   const currentValue = getSpaceValue($space);
-  if (currentValue !== ' ') {
-    const msg = 'Sorry this space is already taken, try another';
-    
+  if (currentValue !== " ") {
+    const msg = "Sorry this space is already taken, try another";
+
     console.log(msg);
     showNewGameError(msg);
     return;
@@ -121,10 +141,10 @@ const onSpaceClick = ($space) => {
 
   logCurrentBoardState();
   showNewGameState();
-  
+
   fullSquares++;
   const isWin = checkForWin();
-  
+
   if (isWin || isStalemate()) {
     return;
   }
@@ -133,33 +153,33 @@ const onSpaceClick = ($space) => {
 };
 
 const createSpace = () => {
-  const $space = document.createElement('div');
-  $space.className = 'game-space';
-  $space.dataset.value = ' ';
+  const $space = document.createElement("div");
+  $space.className = "game-space";
+  $space.dataset.value = " ";
 
-  const $spaceButton = document.createElement('button');
-  $spaceButton.setAttribute('type', 'button');
-  $spaceButton.addEventListener('click', () => onSpaceClick($space));
+  const $spaceButton = document.createElement("button");
+  $spaceButton.setAttribute("type", "button");
+  $spaceButton.addEventListener("click", () => onSpaceClick($space));
   $space.appendChild($spaceButton);
   return $space;
-}
+};
 
 const buildArray = (builderFn, length = 3) => {
   const array = [];
-  for(let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     array.push(builderFn());
   }
   return array;
-}
+};
 
 const buildBoardRow = ($container) => {
   const spaceArray = buildArray(createSpace);
-  const $row = document.createElement('div');
-  $row.className = 'game-row';  
+  const $row = document.createElement("div");
+  $row.className = "game-row";
   $container.appendChild($row);
-  
-  spaceArray.forEach($space => {
-    $row.appendChild($space)
+
+  spaceArray.forEach(($space) => {
+    $row.appendChild($space);
   });
   return spaceArray;
 };
@@ -168,16 +188,16 @@ const buildInitialGameBoard = (gameBoardId) => {
   const $gameBoardContainer = document.getElementById(gameBoardId);
   const rows = buildArray(() => buildBoardRow($gameBoardContainer));
   return rows;
-}
+};
 
 const initClearButton = () => {
-  document.getElementById('clear-board')
-    .addEventListener('click', clearBoard);
-}
+  document.getElementById("clear-board").addEventListener("click", resetGame);
+};
 
 document.body.onload = () => {
-  $gameStateContainer = document.getElementById('game-state');
-  $currentPlayerContainer = document.getElementById('current-player');
-  initClearButton('clear-board');
-  gameBoardState = buildInitialGameBoard('game-board');
+  $gameStateContainer = document.getElementById("game-state");
+  $currentPlayerContainer = document.getElementById("current-player");
+  initClearButton("clear-board");
+  winOptions = buildWinOptions();
+  gameBoardState = buildInitialGameBoard("game-board");
 };
