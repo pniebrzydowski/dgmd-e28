54fc21e8e593f39658cb13dd8878abfbb8ee19cc
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2022-03-07 18:50:07 -0600

    hot dogs v1 - assoc array

---
+++ assignment-03/assoc-array/script.js
@@ -0,0 +1,75 @@
+const itemPrices = new Array();
+itemPrices["hot dogs"] = 400;
+itemPrices["fries"] = 350;
+itemPrices["soda"] = 150;
+itemPrices["sauerkraut"] = 100;
+
+const showOrder = (totalPrice, orderSummary) => {
+  const $orderReview = document.getElementById("order-review");
+  const $orderSummary = document.getElementById("order-summary");
+  const $totalPrice = document.getElementById("total-price");
+
+  if (undefined === totalPrice) {
+    // reset condition
+    $orderSummary.innerHTML = "";
+    $totalPrice.innerHTML = "";
+    $orderReview.classList.add("hidden");
+    return;
+  }
+
+  $orderSummary.innerHTML = orderSummary.join(", ");
+  $totalPrice.innerHTML = `$${(totalPrice / 100).toFixed(2)}`;
+  $orderReview.classList.remove("hidden");
+};
+
+const resetOrderDisplay = () => showOrder();
+
+const getInputFields = ($form) => {
+  return {
+    hotdog: $form.querySelector("input[name=hotdog]"),
+    fries: $form.querySelector("input[name=fries]"),
+    soda: $form.querySelector("input[name=soda]"),
+    sauerkraut: $form.querySelector("input[name=sauerkraut]"),
+  };
+};
+
+const calculateOrder = ($form) => {
+  const inputs = getInputFields($form);
+  const orderQuantities = new Array();
+  orderQuantities["hot dogs"] = parseInt(inputs.hotdog.value, 10);
+  orderQuantities["fries"] = parseInt(inputs.fries.value, 10);
+  orderQuantities["soda"] = parseInt(inputs.soda.value, 10);
+  orderQuantities["sauerkraut"] = parseInt(inputs.sauerkraut.value, 10);
+
+  let totalPrice = 0;
+  let orderSummary = [];
+
+  for (let key in itemPrices) {
+    totalPrice += orderQuantities[key] * itemPrices[key];
+    if (orderQuantities[key] > 0) {
+      orderSummary.push(`${key}: ${orderQuantities[key]}`);
+    }
+  }
+
+  showOrder(totalPrice, orderSummary);
+};
+
+const resetOrder = ($form) => {
+  const inputs = getInputFields($form);
+  for (let key in inputs) {
+    inputs[key].value = 0;
+  }
+  resetOrderDisplay();
+};
+
+const initForm = () => {
+  const $orderForm = document.getElementById("order-form");
+  $orderForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    calculateOrder($orderForm);
+  });
+  const $resetBtn = document.getElementById("reset-btn");
+  $resetBtn.addEventListener("click", () => resetOrder($orderForm));
+};
+
+document.body.onload = initForm;
