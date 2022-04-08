d724328c7289b1f498016248163c9bfe5a3fac9f
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2022-04-08 19:32:25 -0300

    enter multiple guesses & evaluate them

--- assignment-04/script.js
+++ assignment-04/script.js
@@ -1,87 +1,182 @@
 class Letter {
-  constructor(letter) {
+  constructor(letter = "", index = -1) {
     this.letter = letter;
+    this.index = index;
     this.isCorrect = false;
     this.isInWord = false;
   }
   generateElement() {
     const $letterEl = document.createElement("li");
     $letterEl.classList.add("letter");
+    if (this.letter === "") {
+      return $letterEl;
+    }
+    $letterEl.innerHTML = this.letter;
+
     if (this.isCorrect) {
       $letterEl.classList.add("letter-correct");
     }
     if (this.isInWord) {
       $letterEl.classList.add("letter-inword");
     }
-    $letterEl.innerHTML = this.letter;
     return $letterEl;
   }
-  evalute() {}
+  evaluate(wordGuess) {
+    const lettersGuessed = wordGuess.split("");
+    console.log(lettersGuessed, this.letter);
+    if (lettersGuessed.includes(this.letter)) {
+      console.log(lettersGuessed[this.index], this.letter);
+      if (lettersGuessed[this.index] === this.letter) {
+        this.isCorrect = true;
+      } else {
+        this.isInWord = true;
+      }
+    }
+  }
 }
 
 class Word {
   constructor() {
-    this.word = "ILUVU";
-    this.letters = this.word.split("").map((ltr) => new Letter(ltr));
+    this.letters = [];
   }
 
-  generateElement() {
-    const lettersList = this.letters.map((letter) => letter.generateElement().outerHTML);
-    const $el = document.createElement("ul");
-    $el.className = "row";
-    $el.innerHTML = lettersList.join("");
-    return $el;
+  get() {
+    if (this.word) {
+      return this.word;
+    }
+
+    this.word = "ILUVU".toUpperCase();
+    this.letters = this.word.split("").map((ltr, idx) => new Letter(ltr, idx));
+    return this.word;
+  }
+}
+
+class Guess {
+  constructor(index) {
+    this.index = index;
+    this.letterCount = 0;
+    const emptyWord = [];
+    for (let i = 0; i < 5; i++) {
+      emptyWord.push(new Letter());
+    }
+    this.letters = emptyWord;
   }
 
-  evaluate(wordGuess) {
-    if (wordGuess.toUpperCase() === this.word) {
+  getGuessWord() {
+    return this.letters.reduce((word, letter) => (word += letter));
+  }
+
+  evaluate(correctWord) {
+    if (this.getGuessWord() === correctWord) {
       return true;
     }
 
     this.letters.forEach((letter) => {
-      letter.evalute(this.word);
+      letter.evaluate(correctWord);
     });
+    this.updateRow();
+    return false;
+  }
+
+  isFull() {
+    return this.letterCount === 5;
+  }
+
+  updateRow() {
+    this.$row.innerHTML = this.getRow().join("");
+  }
+
+  removeLastLetter() {
+    console.log(this.letters);
+    const length = this.letters.length;
+    this.letters.splice(length, 1);
+    this.letterCount--;
+    this.updateRow();
+  }
+
+  addNewLetter(letter) {
+    const length = this.letterCount;
+    this.letters[length] = new Letter(letter, this.letterCount);
+    this.letterCount++;
+    this.updateRow();
+  }
+
+  getRow() {
+    return this.letters.map((letter) => letter.generateElement().outerHTML);
+  }
+
+  generateElement() {
+    const $el = document.createElement("ul");
+    $el.className = "row";
+    $el.innerHTML = this.getRow().join("");
+    this.$row = $el;
+    return $el;
   }
 }
 
 class Game {
   constructor() {
     this.$gameBoard = document.getElementById("game-board");
+    this.guesses = [1, 2, 3, 4, 5, 6].map((idx) => new Guess(idx));
+    this.guessIndex = 0;
+    this.currentGuess = this.guesses[0];
     this.startGame();
   }
 
-  createNewRow() {
-    const $wordEl = this.word.generateElement();
-    console.log($wordEl);
-    this.$gameBoard.appendChild($wordEl);
+  createEmptyBoard() {
+    this.guesses.forEach((guess) => {
+      const $el = guess.generateElement();
+      this.$gameBoard.appendChild($el);
+    });
   }
 
-  validateWordEntry(word) {
-    if (word.length !== 5) {
-      alert("Your word entry must be 5 letters! Try again");
-      return false;
-    }
-    return true;
+  goToNextGuess() {
+    this.guessIndex++;
+    this.currentGuess = this.guesses[this.guessIndex];
   }
 
-  initForm() {
-    console.log("init form");
-    const $form = this.$gameBoard.querySelector("form");
-    $form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const $input = this.$form.querySelector("input");
-      const value = $input.value;
-      if (!this.validateWordEntry(value)) {
+  initKeyboard() {
+    document.body.addEventListener("keyup", (e) => {
+      const key = e.key;
+      const fullGuess = this.currentGuess.isFull();
+
+      if (key === "Enter") {
+        if (fullGuess) {
+          const isCorrect = this.currentGuess.evaluate(this.word.get());
+          if (isCorrect) {
+            alert("Congratulations, you guessed the word correctly!");
+            return;
+          }
+          this.goToNextGuess();
+          return;
+        }
+
+        alert("You must enter 5 letters before submitting!");
         return;
       }
-      this.word.evalute(value);
+
+      if (key === "Backspace") {
+        this.currentGuess.removeLastLetter();
+        return;
+      }
+
+      if (key.length !== 1 || !/[a-zA-Z]/.test(key)) {
+        console.log("Not a letter", key);
+        return;
+      }
+
+      if (fullGuess) {
+        return;
+      }
+      this.currentGuess.addNewLetter(key.toUpperCase());
     });
   }
 
   startGame() {
     this.word = new Word();
-    this.createNewRow();
-    // this.initForm();
+    this.initKeyboard();
+    this.createEmptyBoard();
+    this.word.get();
   }
 }
 
